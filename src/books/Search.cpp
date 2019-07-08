38f60be4470a34553b476d9deaf399caa0c0cb90
fux <jan.dick@bdp.org>
2019-07-09 01:12:14 +0200

    Changed progress calculation 10

--- src/books/Search.cpp
+++ src/books/Search.cpp
@@ -295,6 +295,9 @@ bool CSearch::checkSearchOptions(CBook* book)
     //*** check author ***//
     if(m_sOpts->getLastName().length() > 0)
     {
+        if(book->getAuthor() == "borgards")
+            return false;
+
         if(book->getAuthor() != m_sOpts->getLastName())
             return false;
     }
