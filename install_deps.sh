9494890710e2212046dc581ed5672c343a926438
Alexander Leonhardt <equinox.salexander@gmail.com>
2019-10-12 11:04:52 +0200

    First python rewrite commit

--- install_deps.sh
+++
@@ -1,8 +0,0 @@
-mkdir build
-mkdir doc
-mkdir bin
-sudo iptables -A PREROUTING -t nat -p tcp --dport 443 -j REDIRECT --to-ports 1409
-sudo iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 1408
-sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 443 -j REDIRECT --to-port 1409
-sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 1408
-sudo apt install iptables-persistent
--- src/util/debug_file.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#ifdef DBG_MSG
-#undef DBG_MSG
-#define DBG_MSG debug::print
-#endif
