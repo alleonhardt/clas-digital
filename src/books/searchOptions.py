8c6e31c63240b77a166643948b88caed0f2b65d7
Alexander Leonhardt <equinox.salexander@gmail.com>
2019-10-22 10:47:48 +0200

    Just created the basic makefile and initialised the basic dependencies and created the basic files

--- src/books/searchOptions.py
+++
@@ -1,9 +0,0 @@
-class CSearchOptions:
-
-    def __init__(self, word, fuzzyness, onlyTitle, onlyOcr):
-        self.word = word
-        self.fuzzyness = fuzzyness
-        self.onlyTitle = onlyTitle
-        self.onlyOcr = onlyOcr
-
-
