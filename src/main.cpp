80ce3f8a96fdff982da8e9dafe07ced7188e173f
Alexander Leonhardt <equinox.salexander@gmail.com>
2019-10-22 15:01:07 +0200

    Added a makefile rule for the books subdirectory

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,12 @@ int main()
 {
 	std::cout<<"Stuff works kind of nice!"<<std::endl;
 	Server srv;
-	srv.Get("/api",[](const Request &, Response &resp) {
+	srv.Get("/api",[](const Request &req, Response &resp) {
+			std::cout<<"Request to: "<<req.path<<std::endl;
+			for(auto &it : req.headers)
+			{
+				std::cout<<it.first<<": "<<it.second<<std::endl;
+			}
 			resp.set_content("Hello World from new c server!", "text/plain");
 			});
 	srv.listen("localhost", 9000);
