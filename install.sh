f595567d1732bc27d29a62ec6dd2fb2d4259608c
Alexander Leonhardt <equinox.salexander@gmail.com>
2019-10-22 11:36:20 +0200

    Added more stable install scripts for easy server installation

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install nginx
+sudo apt-get install cmake
+sudo apt-get install g++
+sudo apt-get install make
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#define CPPHTTPLIB_THREAD_POOL_COUNT 8
+#include "httplib.h"
+
+using namespace httplib;
 
 int main()
 {
 	std::cout<<"Stuff works kind of nice!"<<std::endl;
+	Server srv;
+	srv.Get("/api",[](const Request &, Response &resp) {
+			resp.set_content("Hello World from new c server!", "text/plain");
+			});
+	srv.listen("localhost", 9000);
 	return 0;
 }
