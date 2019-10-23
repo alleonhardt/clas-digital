266f7ccf54eff5d24e891f9fd5165853b0c89636
Alexander Leonhardt <equinox.salexander@gmail.com>
2019-10-23 08:34:06 +0200

    Added parsing of the search query options

--- src/main.cpp
+++ src/main.cpp
@@ -56,16 +56,6 @@ int main()
 	}
 	srchFile.close();
 
-
-	srv.Get("/api",[](const Request &req, Response &resp) {
-			std::cout<<"Request to: "<<req.path<<std::endl;
-			for(auto &it : req.headers)
-			{
-			std::cout<<it.first<<": "<<it.second<<std::endl;
-			}
-			resp.set_content("Hello World from new c server!", "text/plain");
-			});
-
 	srv.Post("/login",[](const Request &req, Response &resp) {
 			URLParser parser(req.body);
 			auto &usrtable = UserHandler::GetUserTable();
@@ -84,42 +74,95 @@ int main()
 			}
 			});
 	srv.Get("/search",[&](const Request &req, Response &resp) {
-			std::cout<<"Request to search to : "<<req.uri<<std::endl;
-			std::string app = fileSearchHtml;
-			app+="<script>let ServerDataObj = {pillars:";
-			app+=zoteroPillars.dump();
-			app+="};</script>";
-			resp.set_content(app.c_str(),"text/html");
-			});
-	srv.Get("/authenticate",[](const Request &req, Response &resp) {
-			
-			int accreq = 1;
-			if(req.path.find("/admin/")!=std::string::npos)
-				accreq = 4;
-			else if(req.path.find("/write/")!=std::string::npos)
-				accreq = 2;
-			
-			resp.status = 403;
-			const char *ptr = get_header_value(req.headers,"Cookie");
-			if(!ptr) return;
-			std::string x = ptr;
-			auto pos = x.find("SESSID=");
-			auto pos2 = x.find(";",pos);
-			std::string cookie = "";
-			if(pos2==std::string::npos)
-				cookie = x.substr(pos+7);
-			else
-				cookie = x.substr(pos+7,pos2);
-			std::cout<<"Cookie found: "<<cookie<<std::endl;
+					if(req.has_param("q"))
+					{
+						try
+						{
+						//This is a search query!
+						std::string query = req.get_param_value("q",0);
+						int fuzz = std::stoi(req.get_param_value("fuzzyness",0));
+						bool auth_only = req.get_param_value("title_only",0)=="true";
+						bool ocr_only = req.get_param_value("ocr_only",0) == "true";
+						std::string author = req.get_param_value("author",0);
+						int pubafter = std::stoi(req.get_param_value("publicatedafter"));
+						int pubbef = std::stoi(req.get_param_value("publicatedbefore"));
+						bool sortway = req.get_param_value("sortway",0) == "sortbyrelevance";
+						std::string pill = req.get_param_value("pillars",0);
+						std::vector<std::string> pillars;
+						auto start = 0;
+						while(true)
+						{
+							auto pos = pill.find(",",start);
+							std::string tmp;
+							if(pos==std::string::npos)
+							{
+								tmp = pill.substr(start);
+							}
+							else
+							{
+								tmp = pill.substr(start,pos);
+							}
+							if(tmp!="")
+								pillars.push_back(std::move(tmp));
+							if(pos==std::string::npos)
+								break;
+							start = pos+1;
+						}
 
-			auto &usrtable = UserHandler::GetUserTable();
-			auto usr = usrtable.GetUserBySessid(cookie);
-			if(usr && ((usr->GetAccessRights()&accreq)==accreq))
-				resp.status = 200;
-				
+						std::cout<<"Receveived search request!"<<std::endl;
+						std::cout<<"Query: "<<query<<"; fuzz: "<<fuzz<<"; author only: "<<auth_only<<"; ocr only: "<<ocr_only<<"; author: "<<author<<"; publicated after: "<<pubafter<<"; publicated before: "<<pubbef<<"; sorting by relevance: "<<sortway<<"; searched pillars: "<<pill<<"; vector pillar size: "<<pillars.size()<<std::endl;
+					
+						std::string app = fileSearchHtml;
+						app+="<script>let ServerDataObj = {pillars:";
+						app+=zoteroPillars.dump();
+						app+="};</script>";
+						resp.set_content(app.c_str(),"text/html");
+						}
+						catch(...)
+						{
+							resp.set_content("<html><head></head><body><h1>Corrupted search request!</h1></body></html>","text/html");
+							return;
+						}
+					}
+					else
+					{
+
+					std::string app = fileSearchHtml;
+					app+="<script>let ServerDataObj = {pillars:";
+					app+=zoteroPillars.dump();
+					app+="};</script>";
+					resp.set_content(app.c_str(),"text/html");
+					}
+					});
+			srv.Get("/authenticate",[](const Request &req, Response &resp) {
+
+				int accreq = 1;
+				if(req.path.find("/admin/")!=std::string::npos)
+				accreq = 4;
+				else if(req.path.find("/write/")!=std::string::npos)
+				accreq = 2;
+
+				resp.status = 403;
+				const char *ptr = get_header_value(req.headers,"Cookie");
+				if(!ptr) return;
+				std::string x = ptr;
+				auto pos = x.find("SESSID=");
+				auto pos2 = x.find(";",pos);
+				std::string cookie = "";
+				if(pos2==std::string::npos)
+				cookie = x.substr(pos+7);
+				else
+				cookie = x.substr(pos+7,pos2);
+				std::cout<<"Cookie found: "<<cookie<<std::endl;
+
+				auto &usrtable = UserHandler::GetUserTable();
+				auto usr = usrtable.GetUserBySessid(cookie);
+				if(usr && ((usr->GetAccessRights()&accreq)==accreq))
+					resp.status = 200;
+
 			});
-	std::cout<<"C++ Api server startup successfull!"<<std::endl;
-	srv.listen("localhost", 9000);
-	return 0;
+			std::cout<<"C++ Api server startup successfull!"<<std::endl;
+			srv.listen("localhost", 9000);
+			return 0;
 }
 
