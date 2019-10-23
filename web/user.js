f21e17c4f5f11916dd36a0f29e744be83bccb7fb
Alexander Leonhardt <equinox.salexander@gmail.com>
2019-10-23 10:29:39 +0200

    Outsourced the topnav creating and link coloring to external files

--- web/user.js
+++ web/user.js
@@ -10,18 +10,13 @@ class User {
 		window.location = window.location;
 	}
 
-	static initialise() {
-		let inp = document.getElementById("SpecialSID");
-		inp.focus();
-		inp.addEventListener("keypress",function(event){
-			if(event.key == "Enter")
-			{
-				document.getElementById("srchButton").click();
-			}
-		});
-		//inp.addEventListener("input",function(event){
-		//	document.getElementById("srchButton").click();
-		//});
+	static initialise(linkforactive) {
+		if(linkforactive==undefined) linkforactive = 'searchlink';
+		if (typeof(ServerDataObj) == 'undefined') {
+			this.ServerDataObj = {};
+		}
+		else
+			this.ServerDataObj = ServerDataObj;
 
 		let vars = document.cookie;
 		console.log("Document cookie: "+ document.cookie);
@@ -37,24 +32,24 @@ class User {
 
 			console.log(cookie);
 			let pos2 = cookie.indexOf("::");
-			ServerDataObj.user = {};
-			ServerDataObj.user.email = cookie.substr(0,pos2);
-			ServerDataObj.user.access = parseInt(cookie.substr(pos2+2,cookie.indexOf(":::")));
+			this.ServerDataObj.user = {};
+			this.ServerDataObj.user.email = cookie.substr(0,pos2);
+			this.ServerDataObj.user.access = parseInt(cookie.substr(pos2+2,cookie.indexOf(":::")));
 		}
 
 		let specval = document.getElementById("tpnav");
-		specval.innerHTML = "<a href='/home'>Home</a><a class='active' href='/search'>Search</a><a class='classifiedContent' href='/administration' data-acc=4>Administration</a><a class='classifiedContent' href='/uploadbook' data-acc=2>Upload Books</a><a class='classifiedContent' href='/managebooks' data-acc=2>Manage Books</a><div class='topnav-right' id='loggedintopnav'><span id='LoggedInAs'>Logged in</span><button id='logoutBut' class='btn btn-outline-info' onclick='User.DoLogout()'>Logout</button></div>";
+		specval.innerHTML = "<a id='homelink' href='/guest_index.html'>Home</a><a id='searchlink' href='/search'>Search</a><a id='administrationlink' class='classifiedContent' href='/private/admin/Administration.html' data-acc=4>Administration</a><a id='uploadbooklink' class='classifiedContent' href='/private/write/UploadBook.html' data-acc=2>Upload Books</a><a id='managebookslink' class='classifiedContent' href='/private/write/ManageBooks.html' data-acc=2>Manage Books</a><div class='topnav-right' id='loggedintopnav'><span id='LoggedInAs'>Logged in</span><button id='logoutBut' class='btn btn-outline-info' onclick='User.DoLogout()'>Logout</button></div>";
 
-		if(ServerDataObj==undefined)
-			alert("Site internal error");
-		else if(ServerDataObj.user == undefined)
+		console.log("Link: " + linkforactive);
+		document.getElementById(linkforactive).classList.add("active");
+		if(this.ServerDataObj.user == undefined)
 		{
 				let x = document.getElementById("loggedintopnav");
 				x.innerHTML = '<form id ="LoginForm" class="form-inline my-2 my-lg-0" action="/login" method="post"><input id="EmailType" class="form-control mr-sm-2" type="email" name="email" placeholder="Email" aria-label="Email" required><input id="PasswordType" class="from-control mr-sm-2" type="password" name="password" placeholder="Password" aria-label="Password" required><input class="btn btn-outline-success my-2 my-sm-0" type="submit" value="Log In"/></form>';
 		}
 		else
 		{
-			document.getElementById("LoggedInAs").innerHTML = "Logged in as <strong>"+ServerDataObj.user.email+"</strong>";
+			document.getElementById("LoggedInAs").innerHTML = "Logged in as <strong>"+this.ServerDataObj.user.email+"</strong>";
 
 			let list = document.getElementsByClassName("classifiedContent");
 
@@ -62,7 +57,7 @@ class User {
 			for(var i=0; i < list.length; i++)
 			{
 				let y = list[i].dataset.acc;
-				if((ServerDataObj.user.access&y)!=0)
+				if((this.ServerDataObj.user.access&y)!=0)
 				{
 					list[i].style.display="block";
 				}
