11cde71ac1a2304f6c0fabaa46d768d7714efc5e
Alexander Leonhardt <equinox.salexander@gmail.com>
2019-11-11 03:57:29 +0100

    Added a new version of the getbooks site

--- web/general.js
+++ web/general.js
@@ -1,1 +1,12 @@
 let gGeneralJavascriptFile = 0;
+
+function getParameterByName(name, url) {
+//COPYRIGHT TO https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+	if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
