88e91ab074b6b5711c97afaea8193127a846170b
Paul Sladen <uni-ffm@paul.sladen.org>
2020-02-15 18:14:18 +0100

    rel=canonical for books/ABC123 metadata Hopefully resolves #170

--- src/util/StaticWebpageCreator.hpp
+++ src/util/StaticWebpageCreator.hpp
@@ -84,7 +84,11 @@ class StaticWebpageCreator
 				//Metadata page only
 				std::string content = "<!doctype html><html lang=\"en\"><head><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\"><link rel=\"shortcut icon\" href=\"static/fav/hand-top-right-16+32+48.ico\"/><title>";
 				content+=m_book->getMetadata().getShow2();
-				content+="</title></head><body style=\"padding: 5rem;\"><h1>";
+				content+="</title>\n";
+				content+="<link rel=\"canonical\" href=\"https://www.clas-digital.uni-frankfurt.de/books/";
+				content+=m_book->getKey();
+                                content+="\"/>\n";
+                                content+="</head><body style=\"padding: 5rem;\"><h1>";
 				content+=m_info["bib"];
 				content+="</h1><nav id='booklcontentlink'><ul><li><a href=\"";
 				content+=webpath;
