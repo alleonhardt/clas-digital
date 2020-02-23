713f1c4a50c80f52239b105e1121ba507299a633
Alexander Leonhardt <equinox.salexander@gmail.com>
2020-02-23 19:46:27 +0000

    Added new changes

--- src/main.cpp
+++ src/main.cpp
@@ -818,8 +818,8 @@ int main(int argc, char **argv)
 	auto last_mod = std::filesystem::last_write_time("bin/zotero.json");
 	int managedBooks = 0;
 
-	//StaticCatalogueCreator ct;
-	//ct.CreateCatalogue();
+	StaticCatalogueCreator ct;
+	ct.CreateCatalogue();
 	std::ofstream of("bin/forbiddenfiles",std::ios::out);
 	if(of.is_open())
 	{
@@ -874,8 +874,6 @@ int main(int argc, char **argv)
 	}
 	of.close();
     }
-   StaticCatalogueCreator ct;
-   ct.CreateCatalogue(); 
 
     int y = system("systemctl restart nginx");
     y+=1;
