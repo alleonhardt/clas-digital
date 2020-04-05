7e24e9450e12d1db9b14aad3679f5065a2bd5195
Alexander Leonhardt <equinox.salexander@gmail.com>
2020-04-05 22:53:17 +0000

    Converted the basic pages function into an template used to rerender the pages site of the books

--- src/util/StaticWebpageCreator.hpp
+++ src/util/StaticWebpageCreator.hpp
@@ -48,6 +48,19 @@ class StaticWebpageCreator
 			ofs.close();
 		}
 
+		void CreatePagesPage()
+		{
+			nlohmann::json js;
+			js["key"] = m_book->getKey();
+			js["title"] = m_book->getMetadata().getShow2();
+			js["bib"] = m_info["bib"];
+			inja::Environment env;
+			inja::Template temp = env.parse_template("web/books/pages_template.html");
+			std::string result = env.render(temp, js);
+			std::ofstream ofs("web/books/pages/"+m_book->getKey()+"/index.html",std::ios::out);
+			ofs<<result;
+			ofs.close();
+		}
 		bool createWebpage()
 		{
 			std::string webpath = "/books/"+m_bookID+"/pages";
@@ -58,55 +71,7 @@ class StaticWebpageCreator
 			//std::filesystem::remove_all(m_path+"/meta",ec);
 			if(std::filesystem::exists(m_path+"/readerInfo.json"))
 			{
-				//Full blown webpage
-				std::string content="<!DOCTYPE html><html><head><title>";
-				content+=m_book->getMetadata().getShow2();
-				content+="</title><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">";
-				content+="<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css\" integrity=\"sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB\" crossorigin=\"anonymous\">";
-				content+="<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css\">";
-				content+="<link rel=\"canonical\" href=\"https://www.clas-digital.uni-frankfurt.de/books/";
-				content+=m_book->getKey();
-				content+="/pages\" />\n";
-				content+="<link rel=\"up\" href=\"https://www.clas-digital.uni-frankfurt.de/books/";
-				content+=m_book->getKey();
-				content+="\" />\n";
-				content+="<link rel=\"top\" href=\"https://www.clas-digital.uni-frankfurt.de/\"/>\n";
-				content+="<link rel=\"shortcut icon\" href=\"/static/fav/hand-top-right-16+32+48.ico\"/>";
-				content+="<link rel=\"stylesheet\" href=\"/static/pages.css\">";
-				content+="<script>let gGlobalBookId=\"";
-				content+=m_book->getKey();
-				content+="\";</script><script src=\"/general.js\"></script><script src=\"/GetBooks.js\"></script></head><body><nav class='searchbox'>\n<a class=\"about\" href=\"../\" rel=\"index up\">About this book</a>\n<center><input id='srchbox' style='width:25%;border-radius: 20px;padding-left: 0.5rem;padding-right: 0.5rem;margin-left: 1rem;min-width: 10rem;margin-top: 0.5rem;text-align: center;' type='text' placeholder=\"search this book\"><i class=\"fa fa-search searchstyler\" onclick='doCompleteNewSearch();return true;'></i></input><img id='fullbut' onclick='tooglefullscreen(this);' class='fullscreen' title='Toogle fullscreen mode' style='float: right; margin:0.5rem' src=\"/static/GetBooks/fullscreen-24px.svg\"/><img id='tooglebut' onclick=\"read_mode(this);\" class='fullscreen' style='float: right;margin:0.5rem;' title=\"Toggle image reader and split reader mode\" src=\"/static/GetBooks/chrome_reader_mode-24px.svg\"/><div style='position: relative;width:90%;height: 1px;'><div id='fuzzysuggestions' style='visibility:hidden;overflow-y:scroll;z-index: 5;position: absolute;top:0;left: 0;right:0;margin-left: auto;margin-right:auto;max-height:80vh;background:white;border: 1px solid black;'></div></div></center><center><span id=\"bibliography\" class=\"bibliostyle\">";
-				content+=m_info["bib"];
-				content+="</span></center><div class='linknav'><div class='lastbutcont'><img class='lastbut' onclick='SelectLastHit();' src='/next.svg'/></div><div id=\"fullsearchhitlist\"></div><div class='nextbutcont'><img class='nextbut' onclick='SelectNextHit();' src='/last.svg'/></div></div></nav></body></html>";
-				
-				std::string pathcopy = m_path;
-				pathcopy+="/pages";
-				std::filesystem::create_directory(pathcopy,ec);
-				
-				//MOVE all jps from web/books/BOOKID/*.jpg to web/books/BOOKID/pages/*.jpg
-			
-				for(auto &it : std::filesystem::directory_iterator(m_path))
-				{
-					auto pos = it.path().string().find(".jpg");
-
-					if(pos!=std::string::npos && pos == it.path().string().length()-4)
-					{
-						std::filesystem::rename(it.path(),m_path+"/pages/"+it.path().filename().string(),ec);
-						if(ec)
-						{
-							std::cout<<"Could not rename "<<it.path()<<std::endl;
-							break;
-						}
-					}
-				}
-
-				
-				pathcopy+="/index.html";
-
-				std::ofstream ofs(pathcopy.c_str(), std::ios::out);
-				ofs<<content;
-				ofs.close();
-
+				CreatePagesPage();
 			}
 			
 
