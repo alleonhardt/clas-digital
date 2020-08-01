b3a3d1167660f042620ca0a54ce63ef4d8802e6d
fux <jan.dick@bdp.org>
2020-08-01 15:02:31 +0200

    Working on documentation

--- src/books/CMetadata.hpp
+++ src/books/CMetadata.hpp
@@ -10,64 +10,74 @@
 class CMetadata
 {
 private:
-    nlohmann::json m_metadata;
+    nlohmann::json m_metadata;  ///< Json with book metadata
 
 public:
     
+    ///Empty constructor, not initializing metadata.
     CMetadata();
 
     /**
-    * @brief constructor
+    * @brief Constructor initializing metadata.
     * @param[in] jMetadata json with metadata
     */
     CMetadata(nlohmann::json jMetadata);
  
     /*
-    * @param[in] jMetadata new metadata
+    * @brief Set metadata, f.e. when json is updated from zotero
+    * @param[in] jMetadata (New metadata)
     */
     void setMetadata(nlohmann::json jMetadata);
 
 
     //************ get metadata *******************//
 
+    // *** GETTER *** //
+
     /**
     * @brief return metadata as json
     */
     nlohmann::json getMetadata();
 
+    
+    // *** GENERAL *** //
+
     /**
-    * @brief getter function to return selected metadata
-    * @parameter string (sSearch: which metadata (f.e. title, date...)
+    * @brief return data from metadata.
+    * @param[in] sSearch (which metadata (f.e. title, date...)
     * @return string 
     */
     std::string getMetadata(std::string sSearch);
     
     /**
-    * @brief getter function to return selected metadata
-    * @parameter string (sSearch: which metadata (f.e. title, date...)
-    * @parameter string (sFrom: from which json (f.e. title -> data -> title) 
+    * @brief return data from metadata.
+    * @param[in] sSearch (which metadata (f.e. title, date...)
+    * @param[in] sFrom (from which json (f.e. title -> data -> title) 
     * @return string 
     */
     std::string getMetadata(std::string sSearch, std::string sFrom);
 
     /**
-    * @brief getter function to return selected metadata
-    * @parameter string (sSearch: which metadata (f.e. title, date...)
-    * @parameter string (sFrom2: from which json (f.e. title -> data -> title) 
-    * @parameter string (sFrom2: in json from which json (f.e. author -> data creators -> author)
+    * @brief return data from metadata
+    * @param[in] sSearch (which metadata (f.e. title, date...)
+    * @param[in] sFrom1 (from which json (f.e. title -> data -> title) 
+    * @param[in] sFrom2 (in json from which json (f.e. author -> data creators -> author)
     * @return string 
     */
     std::string getMetadata(std::string sSearch, std::string sFrom1, std::string sFrom2);
 
     /**
-    * @brief getter function to return selected metadata
-    * @parameter string (sSearch: which metadata (f.e. title, date...)
-    * @parameter string (sFrom2: from which json (f.e. title -> data -> title) 
-    * @parameter string (sFrom2: in json from which json (f.e. author -> data creators -> author)
-    * @parameter int (index: in case of list: which element from list)
+    * @brief return data from metadata
+    * @param[in] sSearch (which metadata (f.e. title, date...)
+    * @param[in] sFrom1 (from which json (f.e. title -> data -> title) 
+    * @param[in] sFrom2 (in json from which json (f.e. author -> data creators -> author)
+    * @param[in] in (in case of list: which element from list)
     * @return string 
     */
     std::string getMetadata(std::string sSearch, std::string sFrom1, std::string sFrom2, int in);
+
+
+    // *** SPECIFIC *** // 
 
     /**
     * @return vector with all collections this book is in
@@ -75,15 +85,18 @@ public:
     std::vector<std::string> getCollections();
 
     /**
-    * @return lastName, or Name of author
+    * @return "lastName", or (if not exists) "name" of author
     */
     std::string getAuthor();
 
+    /**
+    * @return vector of strings in the form: "[lastName/ name], [firstName]"
+    */
     std::vector<std::string> getAuthors();
 
     /**
-    * @param[in] sTag tag to search for
-    * @return return whether book has a certain tag
+    * @param[in] sTag (tag to search for)
+    * @return return whether book has a given tag
     */
     bool hasTag(std::string sTag);
 
@@ -93,17 +106,17 @@ public:
     std::string getTitle();
 
     /**
-    * @return shrottitle of book (titel if not exists)
+    * @return "shortTitle" of book (return "title" if not exists)
     */
     std::string getShortTitle();
 
     /**
-    * @return date or -1 if date does not exists or is currupted
+    * @return date or -1 if date does not exists or is corrupted
     */
     int getDate();
 
     /**
-    * @return string with "[Auhtor], [date]"
+    * @return string with "[lastName/ name], [date]"
     */
     std::string getShow();
 
