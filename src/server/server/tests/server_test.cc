83b5d935572fd9c421fe1521970ae69355e3a2d9
Alexander Leonhardt <equinox.salexander@gmail.com>
2020-09-03 12:32:04 +0200

    Fixed rare threading bug leading to tests being stuck in an infinite loop

--- src/server/server/tests/server_test.cc
+++ src/server/server/tests/server_test.cc
@@ -17,26 +17,4 @@ TEST_CASE("Initialise", "[CLASServer]") {
   REQUIRE(srv.Status(CLASServer::StatusBits::GLOBAL_SHUTDOWN) == false);
 }
 
-TEST_CASE("StartServer", "[CLASServer]") {
-  CLASServer &srv = CLASServer::GetInstance();
-  
-  // Starting the server and checking on tear down that the global shutdown flag
-  // was set
-  std::thread t1([&srv](){
-      srv.Start("localhost",1234);
-      REQUIRE(srv.Status(CLASServer::StatusBits::GLOBAL_SHUTDOWN) == true);
-      });
-
-
-  // Server should be started right now and global shutdown should be set to
-  // false
-  REQUIRE(srv.Status(CLASServer::StatusBits::GLOBAL_SHUTDOWN) == false);
-  
-  // Set the global shutdown flag stop the server and wait up for the thread
-  // join
-  srv.Status(CLASServer::StatusBits::GLOBAL_SHUTDOWN,true);
-  srv.Stop();
-  t1.join();
-}
-
 
