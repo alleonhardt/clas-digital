fa519ae166b8a5b939287ce99902d4aa8f22b89e
Alexander Leonhardt <equinox.salexander@gmail.com>
2020-09-03 10:53:56 +0200

    Fixing test error on windows try to fix linking error in Ubuntu 20

--- src/server/login/tests/users_test.cc
+++ src/server/login/tests/users_test.cc
@@ -5,8 +5,11 @@ TEST_CASE("Creating Database","[UserTable]") {
   std::error_code ec;
   std::filesystem::remove("users.db",ec);
 
-  UserTable tbl;
-  REQUIRE(tbl.Load("users.db") == true);
+  {
+    UserTable tbl;
+    REQUIRE(tbl.Load("users.db") == true);
+    //Let the destructor run to write changes on disk!
+  }
 
   std::filesystem::remove("users.db",ec);
   REQUIRE(ec.value() == 0); 
