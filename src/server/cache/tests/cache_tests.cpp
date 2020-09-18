ac9d257bf15efe80a889ced3fffe5e958c94f540
Alexander Leonhardt <equinox.salexander@gmail.com>
2020-09-18 19:16:50 +0200

    Improved the gitignore to ignore metadata from clangd, added a few threading tests for the FixedSizeCache.

--- src/server/cache/tests/cache_tests.cpp
+++ src/server/cache/tests/cache_tests.cpp
@@ -1,5 +1,7 @@
 #include <cache/cache.h>
 #include <catch2/catch.hpp>
+#include <memory>
+#include <thread>
 
 using namespace clas_digital;
 TEST_CASE("Test the constructor","[cache]")
@@ -24,4 +26,64 @@ TEST_CASE("Test Unregister","[cache]")
   cache.erase("file");
   REQUIRE( cache.size() == 0 );
   cache.load("file",[](void*,long long){},[](int err){REQUIRE(err == 2);});
+}
+
+TEST_CASE("Test Cache multiple threads add","[cache]")
+{
+  FixedSizeCache<std::string> cache(1024*1024);
+  
+  std::thread t1([&cache](){
+      for(int i = 0; i < 1000; i++)
+      {
+        cache.insert("file"+std::to_string(i), std::make_unique<UnmutableCacheableFile>("conaninfo.txt"));
+      }
+      });
+
+  std::thread t2([&cache](){
+      for(int i = 0; i < 1000; i++)
+      {
+        cache.insert("file"+std::to_string(i), std::make_unique<UnmutableCacheableFile>("conaninfo.txt"));
+      }
+      });
+ 
+  std::thread t3([&cache](){
+      for(int i = 0; i < 1000; i++)
+      {
+        cache.insert("file"+std::to_string(i), std::make_unique<UnmutableCacheableFile>("conaninfo.txt"));
+      }
+      });
+  t1.join();
+  t2.join();
+  t3.join();
+  REQUIRE( cache.size() == 1000 );
+}
+
+TEST_CASE("Test Cache multiple threads add diffrent","[cache]")
+{
+  FixedSizeCache<std::string> cache(1024*1024);
+  
+  std::thread t1([&cache](){
+      for(int i = 0; i < 1000; i++)
+      {
+        cache.insert("file"+std::to_string(i), std::make_unique<UnmutableCacheableFile>("conaninfo.txt"));
+      }
+      });
+
+  std::thread t2([&cache](){
+      for(int i = 2000; i < 3000; i++)
+      {
+        cache.insert("file"+std::to_string(i), std::make_unique<UnmutableCacheableFile>("conaninfo.txt"));
+      }
+      });
+ 
+  std::thread t3([&cache](){
+      for(int i = 4000; i < 5000; i++)
+      {
+        cache.insert("file"+std::to_string(i), std::make_unique<UnmutableCacheableFile>("conaninfo.txt"));
+      }
+      });
+  t1.join();
+  t2.join();
+  t3.join();
+  REQUIRE( cache.size() == 3000 );
 }
